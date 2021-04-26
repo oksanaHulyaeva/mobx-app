ac9ab6e67d23a65bf9fcb7d553e624862dfad6db
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-04-26 03:32:05 +0300

    docs: init docs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class App extends Component  {
+
+  render() {
+    return (
+        <div className={"app"}>
+          <h1>Introduce yourself, please :)</h1>
+        </div>
+    )
+  }
+}
