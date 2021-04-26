18f6f24832424befc835172749dfaa3893fd7172
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-04-26 13:47:16 +0300

    feat: add store

---
+++ src/store/Store.ts
@@ -0,0 +1,28 @@
+import { observable, computed, action } from 'mobx';
+
+export interface IStore {
+  firstname: string,
+  lastname: string,
+  fullName: () => string,
+  changeFirstname: () => void,
+  changeLastname: () => void
+}
+
+class Store {
+  @observable firstName = 'John';
+  @observable lastName = 'Smith';
+
+  @computed get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  @action changeFirstname (value:string) {
+    this.firstName = value;
+  }
+
+  @action changeLastname (value:string) {
+    this.lastName = value;
+  }
+}
+
+export const appStore = new Store();
