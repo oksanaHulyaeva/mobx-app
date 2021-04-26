e2873c44eb5e8ced541e26060f353e5bce7656c1
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-04-26 17:34:06 +0300

    docs: add sass docs

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import './index.css';
+import './index.scss';
 
 const root = document.getElementById('root')
 
